111486e58d144ff4b83ad32c39952824919f0c83
David Mitchell <jdavidmitchell@gmail.com>
2021-02-08 21:11:05 -0500

    Adding comments

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 #!/usr/bin/env node
 require('dotenv').config();
 import axios from 'axios';
-import { createTransport } from 'nodemailer';
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
-
-
 const apiKey = process.env.APIKEY
 if (!apiKey) {
   throw new Error('No APIKEY');
@@ -15,17 +12,19 @@ if (!accountSid || !authToken) {
     throw new Error('TWILIO_ACCOUNT_SID or AUTH_TOKEN is not defined');
 }
 
+// Use APIKEY to access etherscan.io
 var url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + apiKey
 
 
 axios.get(url)
     .then((response) => {
         const safeGasPrice = response.data.result.SafeGasPrice
-        // console.log(`Your Safe Gas Price is ${safeGasPrice}`)
+        // console.log(`The Safe Gas Price is ${safeGasPrice}`)
         if (!safeGasPrice) {
             throw new Error('No Safe Gas Price');
         } else {
             if (safeGasPrice < 70) {
+                // Send SMS message via twilio
                 client.messages
                     .create({
                         body: 'Safe Gas Price is: ' + safeGasPrice,
