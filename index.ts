65b5ab963786e1d5c8830c5f5f73f8df2e60a2d0
David Mitchell <jdavidmitchell@gmail.com>
2021-02-08 20:42:43 -0500

    Adding twilio

--- index.ts
+++ index.ts
@@ -2,53 +2,37 @@
 require('dotenv').config();
 import axios from 'axios';
 import { createTransport } from 'nodemailer';
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
 
 const apiKey = process.env.APIKEY
 if (!apiKey) {
   throw new Error('No APIKEY');
 }
-const user = process.env.USER
-const password = process.env.PASS
-if (!user || !password) {
-    throw new Error('USER or PASS is not defined');
+if (!accountSid || !authToken) {
+    throw new Error('TWILIO_ACCOUNT_SID or AUTH_TOKEN is not defined');
 }
 
 var url = 'https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=' + apiKey
 
-var transporter = createTransport({
-    host: "mail.moderatedgroups.com",
-      port: 587,
-      //secure: false,
-      //tls: {
-        // do not fail on invalid certs
-      //  rejectUnauthorized: false
-      //},
-      auth: {
-        user: user,
-        pass: password,
-      },
-});
 
 axios.get(url)
     .then((response) => {
         const safeGasPrice = response.data.result.SafeGasPrice
-        console.log(`Your Safe Gas Price is ${safeGasPrice}`)
+        // console.log(`Your Safe Gas Price is ${safeGasPrice}`)
         if (!safeGasPrice) {
             throw new Error('No Safe Gas Price');
         } else {
-            if (safeGasPrice < 200) {
-                transporter.sendMail({
-                    from: user,
-                    to: user,
-                    subject: 'Safe Gas Price is below 200',
-                    text: 'The Safe Gas Price is: ' + safeGasPrice
-                }, function(error, info) {
-                    if (error) {
-                      console.log(error);
-                    } else {
-                      console.log('Email sent: ' + info.response);
-                    }
-                });
+            if (safeGasPrice < 70) {
+                client.messages
+                    .create({
+                        body: 'Safe Gas Price is: ' + safeGasPrice,
+                        from: '+12398808970',
+                        to: '+15715946171‬'
+                    })
+                    .then(message => console.log(message.dateCreated + ', ' + message.body + ', ' + message.sid));
             }
         }
     })
